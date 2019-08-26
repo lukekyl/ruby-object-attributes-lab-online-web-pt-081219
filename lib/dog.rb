fc149383b0a025b6235cafa5155ef9150b4fca8a
Kyle Luke <th.oldschool@gmail.com>
2019-08-26 20:35:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,18 @@ class Dog
   def initiate(name)
     @name = name 
   end
+  def name=(new_name)
+    @name = new_name
+  end
   def name 
     @name
   end
-  def name=(new_name)
-    @name = new_name
+  
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
   end
   
 end
